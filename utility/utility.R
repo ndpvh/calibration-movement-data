6153649a6b242e6d4910827acd3eddffdf6605eb
ndpvh <niels.vanhasbroeck@gmail.com>
2023-10-27 11:34:06 +0200

    Create a utility file for all utility functions

---
+++ utility/utility.R
@@ -0,0 +1,16 @@
+# First, get all libraries read in
+library(tidyverse)
+library(data.table)
+library(ggpubr)
+
+# Get all filenames within the "utility" directory, except, of course, 
+# utility.R itself
+filenames <- list.files(file.path("utility"),
+                        pattern = ".R",
+                        full.names = TRUE,
+                        recursive = TRUE)
+filenames <- filenames[filenames != "utility/utility.R"]
+
+# Loop over these filenames and load in all functions that they define
+lapply(filenames, source) %>% 
+    invisible()
