59efe084eb58483fa10bd7e3ee76def64ae6f86a
ndpvh <niels.vanhasbroeck@gmail.com>
2024-05-21 16:28:40 +0200

    Import pipe from magrittr

--- R/utility.R
+++ R/utility.R
@@ -1,3 +1,6 @@
+# Imports from other packages
+#' @importFrom magrittr %>%
+
 # A vectorized sequence function
 multi_seq <- Vectorize(seq.default, 
                        vectorize.args = c("from", "to", "by", "length.out"))
