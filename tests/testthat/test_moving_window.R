ece45aebc7049cd94eca67898c2323d713435e58
ndpvh <niels.vanhasbroeck@gmail.com>
2024-05-22 15:32:48 +0200

    Add average and weighted average

--- tests/testthat/test_moving_window.R
+++ tests/testthat/test_moving_window.R
@@ -11,7 +11,7 @@ testthat::test_that("Moving window: No grouping", {
         setNames(c("time", "id", "x", "y"))
 
     # Create function that will take the mean of a given bin
-    average <- function(x) {
+    local_average <- function(x) {
         matrix(c(mean(x$x), mean(x$y)), 
                nrow = 1) %>% 
             as.data.frame() %>% 
@@ -20,9 +20,9 @@ testthat::test_that("Moving window: No grouping", {
     }
 
     # Apply the moving average for several cases
-    tst_0 <- nameless::moving_window(data, span = 0, fx = average)
-    tst_1 <- nameless::moving_window(data, span = 1, fx = average)
-    tst_2 <- nameless::moving_window(data, span = 2, fx = average)
+    tst_0 <- nameless::moving_window(data, span = 0, fx = local_average)
+    tst_1 <- nameless::moving_window(data, span = 1, fx = local_average)
+    tst_2 <- nameless::moving_window(data, span = 2, fx = local_average)
 
     # Create the references
     ref_0 <- cbind(x = 1:10, y = 1:10) %>% 
@@ -60,7 +60,7 @@ testthat::test_that("Moving window: Grouping", {
         setNames(c("time", "id", "x", "y"))
 
     # Create function that will take the mean of a given bin
-    average <- function(x) {
+    local_average <- function(x) {
         matrix(c(mean(x$x), mean(x$y)), 
                nrow = 1) %>% 
             as.data.frame() %>% 
@@ -69,9 +69,9 @@ testthat::test_that("Moving window: Grouping", {
     }
 
     # Apply the moving average for several cases
-    tst_0 <- nameless::moving_window(data, span = 0, fx = average, .by = "id")
-    tst_1 <- nameless::moving_window(data, span = 1, fx = average, .by = "id")
-    tst_2 <- nameless::moving_window(data, span = 2, fx = average, .by = "id")
+    tst_0 <- nameless::moving_window(data, span = 0, fx = local_average, .by = "id")
+    tst_1 <- nameless::moving_window(data, span = 1, fx = local_average, .by = "id")
+    tst_2 <- nameless::moving_window(data, span = 2, fx = local_average, .by = "id")
 
     # Create the references
     ref_0 <- cbind(x = 1:10, y = 1:10) %>% 
@@ -135,4 +135,98 @@ testthat::test_that("Moving window: Average", {
 
     # Tests
     testthat::expect_equal(tst, ref)
+})
+
+# Moving average
+testthat::test_that("Moving window: Weighted average", {
+    
+    # Create different predictable data sets
+    data <- list(cbind(rep(1, each = 5),
+                       rep(1, each = 5), 
+                       1:5,
+                       6:10,
+                       1:5), 
+                 cbind(rep(1, each = 5), 
+                       rep(1, each = 5), 
+                       rep(1, each = 5), 
+                       rep(2, each = 5),
+                       1:5),
+                 cbind(rep(1, each = 5), 
+                       rep(1, each = 5), 
+                       c(1, 1, NA, 1, 1), 
+                       c(1, 2, 3, 4, NA),
+                       1:5))
+
+    ########################
+    # Test error
+
+    testthat::expect_error(nameless::weighted_average(data, weights = rep("test", 5)))
+    testthat::expect_error(nameless::weighted_average(data, weights = rep("test", 3)))
+    testthat::expect_error(nameless::weighted_average(data, weights = 1))
+
+    ########################
+    # Test with numeric
+
+    # Symmetric weights
+    weights <- c(0.10, 0.20, 0.40, 0.20, 0.10)
+
+    tst <- lapply(1:3, 
+                  \(i) data[[i]] %>%  
+                      as.data.frame() %>% 
+                      setNames(c("time", "id", "x", "y", "index")) %>% 
+                      nameless::weighted_average(weights = weights))
+
+    ref <- list(data.frame(x = 3, y = 8),
+                data.frame(x = 1, y = 2), 
+                data.frame(x = as.numeric(NA), y = as.numeric(NA)))
+
+    testthat::expect_equal(tst, ref)
+
+    # Asymmetric weights
+    weights <- c(0.10, 0.20, 0.40, 0.60, 0.70)
+
+    tst <- lapply(1:3, 
+                  \(i) data[[i]] %>%  
+                      as.data.frame() %>% 
+                      setNames(c("time", "id", "x", "y", "index")) %>% 
+                      nameless::weighted_average(weights = weights))
+
+    ref <- list(data.frame(x = 3.8, y = 8.8),
+                data.frame(x = 1, y = 2), 
+                data.frame(x = as.numeric(NA), y = as.numeric(NA)))
+
+    testthat::expect_equal(tst, ref)
+
+    ########################
+    # Test with function
+
+    # By index
+    weights <- \(x) x
+
+    tst <- lapply(1:3, 
+                  \(i) data[[i]] %>%  
+                      as.data.frame() %>% 
+                      setNames(c("time", "id", "x", "y", "index")) %>% 
+                      nameless::weighted_average(weights = weights, .by = "index"))
+
+    ref <- list(data.frame(x = 3.67, y = 8.67),
+                data.frame(x = 1, y = 2), 
+                data.frame(x = as.numeric(NA), y = as.numeric(NA)))
+
+    testthat::expect_equal(tst, ref, tolerance = 1e-2)            
+    
+    # By time 
+    weights <- \(x) x
+
+    tst <- lapply(1:3, 
+                  \(i) data[[i]] %>%  
+                      as.data.frame() %>% 
+                      setNames(c("time", "id", "x", "y", "index")) %>% 
+                      nameless::weighted_average(weights = weights, .by = "time"))
+
+    ref <- list(data.frame(x = 3, y = 8),
+                data.frame(x = 1, y = 2), 
+                data.frame(x = as.numeric(NA), y = as.numeric(NA)))
+
+    testthat::expect_equal(tst, ref)    
 })
