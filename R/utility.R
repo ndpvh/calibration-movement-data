279c5b2db7d01c841b8f0a42ef3e944783495163
ndpvh <niels.vanhasbroeck@gmail.com>
2024-05-22 15:38:20 +0200

    Add %notin% as a function

--- R/utility.R
+++ R/utility.R
@@ -4,3 +4,6 @@
 # A vectorized sequence function
 multi_seq <- Vectorize(seq.default, 
                        vectorize.args = c("from", "to", "by", "length.out"))
+
+# A negated %in% function
+`%notin%` <- Negate(`%in%`)
