45a5c707d7a577ceb564ba0e7d8b76f34d67c183
ndpvh <niels.vanhasbroeck@gmail.com>
2024-07-04 14:00:07 +0200

    Bug fix: Columns doubled

--- R/moving_window.R
+++ R/moving_window.R
@@ -86,15 +86,15 @@ moving_window <- function(data,
                                      dplyr::mutate(index = index - obs, 
                                                    relative_time = time - time[obs]) %>% 
                                      dplyr::arrange(time) %>% 
-                                     tidyr::nest()) %>% 
-            dplyr::ungroup() %>% 
+                                     list()) %>% 
             cbind(individual_data) %>% 
-            dplyr::select(-x, -y)
+            dplyr::select(from:obs, time, id, data) %>%
+            dplyr::ungroup()
 
         # Apply the specified function to the selected data
         all_data[[i]] <- individual_data %>% 
             dplyr::rowwise() %>% 
-            dplyr::mutate(data = data$data %>% 
+            dplyr::mutate(data = data %>% 
                                      as.data.frame() %>% 
                                      fx()) %>% 
             tidyr::unnest(data) %>% 
@@ -103,67 +103,10 @@ moving_window <- function(data,
 
     # Bind all data together and sort based on time
     data <- do.call("rbind", all_data) %>% 
-        dplyr::select(-from, -to, -obs, -index) %>% 
-        dplyr::select(time:id, x:y) %>% 
+        dplyr::select(-from, -to, -obs) %>% 
+        dplyr::relocate(time:id, x:y) %>% 
         dplyr::arrange(time) %>% 
         as.data.frame()    
 
     return(data)
-}
-
-
-
-
-
-################################################################################
-# STATISTICS TO USE WITH THE MOVING WINDOW
-
-#' Average observations within a moving window
-#' 
-#' @param data Dataframe that contains the columns `time`, `id`, `x`, and `y`.
-#' 
-#' @return Single-row dataframe with transformed columns `x` and `y`
-#' 
-#' @export
-average <- function(data) {
-    data.frame(x = mean(data$x), 
-               y = mean(data$y)) %>% 
-        return()
-}
-
-#' Average observations within a moving window
-#' 
-#' @param data Dataframe that contains the columns `time`, `id`, `x`, and `y`.
-#' @param weights Either a numeric vector of weights or a function that creates 
-#' the weights to be assigned to each of the data points. Defaults to 
-#' `\(x) dnorm(x, mean = 0, sd = 1)`
-#' @param .by String denoting the column by which the weights should be determined
-#' (i.e., of which the values are passed on to `weights`). Ignored if a numeric 
-#' is provided for `weights`. Defaults to `"index"`.
-#' 
-#' @return Single-row dataframe with transformed columns `x` and `y`
-#' 
-#' @export
-weighted_average <- function(data, 
-                             weights = \(x) dnorm(x, mean = 0, sd = 1), 
-                             .by = "index") {
-    
-    # Create the weights and normalize them (should sum to 1). Depends on whether
-    # the weights are a numeric vector or a function
-    if(class(weights) == "function") {
-        weights <- weights(data[, .by])
-    } else if(class(weights) != "numeric") {
-        stop("Weights should either by numeric or a function.")
-    }
-    weights <- weights / sum(weights)
-
-    # Throw error if the weights are not of equal length to the data
-    if(length(weights) != nrow(data)) {
-        stop("Weights are not of same size as the data.")
-    }
-
-    # Compute the weighted means
-    data.frame(x = sum(data$x * weights), 
-               y = sum(data$y * weights)) %>% 
-        return()
 }
