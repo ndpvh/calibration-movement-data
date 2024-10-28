f40736ed6e84bfaed6acd56b24b035e0a673d86d
ndpvh <niels.vanhasbroeck@gmail.com>
2024-10-28 16:00:15 +0100

    Add LOESS to preprocessing

---
+++ R/loess.R
@@ -0,0 +1,27 @@
+#' Filter data through LOESS
+#' 
+#' Uses the \code{loess} function in R to filter the data. Is a generalization 
+#' and improvement of \code{linear} and \code{parabola} used with the 
+#' \code{moving_window}.
+#' 
+#' @param data Dataframe that contains the columns `time`, `id`, `x`, and `y`.
+#' @param degree Integer denoting the degree of the polynomial. 
+#' 
+#' @return Single-row dataframe with transformed columns `x` and `y`
+#' 
+#' @export
+local_regression <- function(data, 
+                             degree = 1) {
+    
+    # Perform a loess of a given degree
+    result <- loess(formula = y ~ x, 
+                    data = data, 
+                    degree = degree)
+
+    # Once done, we can replace the results of the data with these results
+    result <- data %>%
+        dplyr::mutate(x = result$x, 
+                      y = result$y)
+
+    return(result)
+}
